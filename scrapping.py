ba4e6c1bd1d551498e99a02770069230789bef30
pharaoph09 <1850904176@qq.com>
2024-11-21 10:42:06 +0100

    limit 50

--- scrapping.py
+++ scrapping.py
@@ -9,4 +9,4 @@ keywords_df = pd.read_csv(file_path)
 # Créer un dossier pour sauvegarder les fichiers HTML
 os.makedirs("html_pages", exist_ok=True)
 
-scrape_webpages_to_db(keywords_df)
+scrape_webpages_to_db(keywords_df, max_links=50)
