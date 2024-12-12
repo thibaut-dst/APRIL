004cdad1b5d2ad1531f8606f6636900f9de42ddc
abetbeder <betbeder.antoine@outlook.fr>
2024-12-12 21:23:23 +0100

    Antoine API Front

--- app.py
+++ app.py
@@ -136,6 +136,50 @@ def get_logs():
         return jsonify({'logs': logs[-20:]})  # Return the last 20 lines of logs
     else:
         return jsonify({'logs': []})
+    
+@app.route('/search-documents', methods=['POST'])
+def search_documents():
+    log_file_path = 'pipeline.log'
+    data = request.json
+    query = {}
+
+    # Construire la requête MongoDB
+    and_conditions = []
+
+    # Recherche dans 'keyword' pour les mots de localisation et d'analyse
+    if 'keyword' in data and data['keyword']:
+        words = data['keyword'].split()  # Divise par mot clé si nécessaire
+        for word in words:
+            and_conditions.append({"keyword": {"$regex": word, "$options": "i"}})
+
+    if 'location' in data and data['location']:
+        words = data['location'].split()  # Divise par mot clé si nécessaire
+        for word in words:
+            and_conditions.append({"keyword": {"$regex": word, "$options": "i"}})
+
+    # Ajoutez la condition combinée dans la requête principale
+    if and_conditions:
+        query = {"$and": and_conditions}
+
+    # Recherche dans 'Title_updated' (si fourni)
+    if 'title' in data and data['title']:
+        query["Title_updated"] = {"$regex": data['title'], "$options": "i"}
+
+    # Afficher la requête dans la console pour debug
+    print("Requête envoyée à MongoDB:", query, flush=True)
+
+    # Écrire la requête dans un fichier log
+    with open(log_file_path, 'a') as log_file:
+        log_file.write(f"Requête envoyée à MongoDB: {query}\n")
+
+    # Chercher les documents correspondants
+    try:
+        documents = list(mongo_collection.find(query, {'_id': 0, 'Title_updated': 1, 'domain': 1, 'keyword': 1}))
+        print(f"Documents trouvés: {len(documents)}", flush=True)
+        return jsonify(documents)
+    except Exception as e:
+        print(f"Erreur lors de la recherche : {e}", flush=True)
+        return jsonify({"error": str(e)}), 500
 
 
 #===================== Frontend routing =====================
